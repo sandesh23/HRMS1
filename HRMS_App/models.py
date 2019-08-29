e30c9032c2252fa35581935c4d32fb643c5182aa
Sandesh Mahamure <s.mahamure@gmail.com>
2019-08-29 10:18:07 +0530

    remove unwanted code

--- HRMS_App/models.py
+++ HRMS_App/models.py
@@ -3,4 +3,15 @@ from django.db import models
 # Create your models here.
 
 
-
+class BasicInfo(models.Model):
+    ''' nic = national identification code '''
+    emp_id = models.CharField(primary_key=True, max_length=50)
+    first_name = models.CharField(max_length=50, null=False)
+    middle_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    email_id = models.EmailField(null=False, unique=True)
+    nic = models.CharField(max_length=16, unique=True,null=False)
+    added_by = models.CharField(max_length=50, null=False)
+    added_time = models.DateTimeField(auto_now_add=True, blank=True)
+    modified_by = models.CharField(max_length=50, null=False)
+    modified_time = models.DateTimeField(auto_now_add=True, blank=True)
