e67e627c4189341fc68a71b76ce168097f061168
JohnWick-1 <sbkapte1@gmail.com>
2019-08-29 12:23:43 +0530

    Revert: Merge branch 'Sandesh_demo' of https://github.com/sandesh23/HRMS1 into Sandesh_demo

--- HRMS_App/models.py
+++ HRMS_App/models.py
@@ -16,3 +16,92 @@ class BasicInfo(models.Model):
     added_time = models.DateTimeField(auto_now_add=True, blank=True)
     modified_by = models.CharField(max_length=50, null=False)
     modified_time = models.DateTimeField(auto_now_add=True, blank=True)
+    is_deleted = models.BooleanField(default=True, null=False)
+
+
+class Department(models.Model):
+    dept_id = models.IntegerField(primary_key=True)
+    dept_name = models.CharField(max_length=50, null=False, unique=True)
+    added_by = models.CharField(max_length=50, null=False)
+    added_time = models.DateTimeField(auto_now_add=True, blank=True)
+    modified_by = models.CharField(max_length=50, null=False)
+    modified_time = models.DateTimeField(auto_now_add=True, blank=True)
+    is_deleted = models.BooleanField(default=True, null=False)
+
+
+class Role(models.Model):
+    role_id = models.IntegerField(primary_key=True)
+    role_name = models.CharField(max_length=50, null= False)
+    added_by = models.CharField(max_length=50, null=False)
+    added_time = models.DateTimeField(auto_now_add=True, blank=True)
+    modified_by = models.CharField(max_length=50, null=False)
+    modified_time = models.DateTimeField(auto_now_add=True, blank=True)
+    is_deleted = models.BooleanField(default=True, null=False)
+
+
+class Degree(models.Model):
+    degree_id = models.IntegerField(primary_key=True)
+    degree_name = models.CharField(max_length=50, null= False)
+    added_by = models.CharField(max_length=50, null=False)
+    added_time = models.DateTimeField(auto_now_add=True, blank=True)
+    modified_by = models.CharField(max_length=50, null=False)
+    modified_time = models.DateTimeField(auto_now_add=True, blank=True)
+    is_deleted = models.BooleanField(default=True, null=False)
+
+
+class state(models.model):
+    state_id = models.IntegerField(primary_key=True)
+    state_name = models.CharField(max_length=50, null=False)
+
+
+class Designation(models.model):
+    designation_id = models.IntegerField(primary_key=True)
+    designation_name = models.CharField(max_length=50, null=False)
+    added_by = models.CharField(max_length=50, null=False)
+    added_time = models.DateTimeField(auto_now_add=True, blank=True)
+    modified_by = models.CharField(max_length=50, null=False)
+    modified_time = models.DateTimeField(auto_now_add=True, blank=True)
+    is_deleted = models.BooleanField(default=True, null=False)
+
+    
+class Location(models.Model):
+    location_id = models.CharField(primary_key=True,max_length=50)
+    location_name =models.CharField(max_length=50,null=False)
+    added_by=models.CharField(max_length=50,null=False)
+    added_time=models.DateTimeField(auto_now_add=True,null=False)
+    modified_by=models.CharField(max_length=50,null=False)
+    modified_time=models.TimeField(auto_now_add=True,null=False)
+    is_deleted=models.BooleanField(default=True)
+
+class SourceOfHire(models.Model):
+    source_id=models.CharField(primary_key=True,max_length=50)
+    source_name=models.CharField(max_length=50,null=False)
+    added_by=models.CharField(max_length=50,null=False)
+    added_time=models.DateTimeField(auto_now_add=True,blank=True)
+    modified_by=models.CharField(max_length=50,null=False)
+    modified_time=models.TimeField(auto_now_add=True,null=False)
+    is_deleted=models.BooleanField(default=True)
+
+class EmployeeType(models.Model):
+    emp_id =models.CharField(primary_key=True,max_length=50)
+    emp_type =models.CharField(max_length=50,null=False)
+    added_by = models.CharField(max_length=50, null=False)
+    added_time = models.DateTimeField(auto_now_add=True, blank=True)
+    modified_by = models.CharField(max_length=50, null=False)
+    modified_time = models.TimeField(auto_now_add=True, null=False)
+    is_deleted = models.BooleanField(default=True)
+
+class job_title(models.Model):
+    job_id = models.CharField(primary_key=True, max_length=50)
+    job_title = models.CharField(max_length=50, null=False)
+    added_by = models.CharField(max_length=50, null=False)
+    added_time = models.DateTimeField(auto_now_add=True, blank=True)
+    modified_by = models.CharField(max_length=50, null=False)
+    modified_time = models.TimeField(auto_now_add=True, null=False)
+    is_deleted = models.BooleanField(default=True)
+
+class country(models.Model):
+    country_id = models.CharField(primary_key=True, max_length=50)
+    country_name = models.CharField(max_length=50, null=False)
+
+
