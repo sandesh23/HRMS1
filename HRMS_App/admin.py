b0c1be9417e0e5135da109a7242ed1b4d6713485
satish <satish.raut777@gmail.com>
2019-08-30 11:38:39 +0530

    admin register

--- HRMS_App/admin.py
+++ HRMS_App/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(BasicInfo)
+admin.site.register(Department)
+admin.site.register(Role)
+admin.site.register(Degree)
+admin.site.register(State)
+admin.site.register(Designation)
+admin.site.register(Location)
+admin.site.register(SourceOfHire)
+admin.site.register(EmployeeType)
+admin.site.register(JobTitle)
+admin.site.register(Country)
+admin.site.register(PersonalDetails)
+admin.site.register(WorkExperience)
+admin.site.register(Education)
+admin.site.register(WorkDetails)
+admin.site.register(Summary)
+admin.site.register(Dependant)
