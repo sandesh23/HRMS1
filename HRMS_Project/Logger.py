adf64b4aea212875526510c476e3b27633759614
Sandesh Mahamure <s.mahamure@gmail.com>
2019-09-07 15:40:16 +0530

    Added Logger configuration file

--- HRMS_Project/Logger.py
+++ HRMS_Project/Logger.py
@@ -3,7 +3,7 @@ from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger('my_logger')
 logger.setLevel(logging.DEBUG)
-handler = RotatingFileHandler('D://HRMS.log')
+handler = RotatingFileHandler('D://Logs//HRMS.log')
 formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
 logger.addHandler(handler)
 handler.setFormatter(formatter)
